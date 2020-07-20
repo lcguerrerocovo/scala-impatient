3603bfa34affe0b27856b204cc54e86d7aa4a3c3
Luis Guerrero <lcguerrerocovo@gmail.com>
2020-07-20 21:33:22 +0200

    adding solutions to chapter 9 exercises

--- src/main/scala/impatient_exercises/Chapter09.scala
+++ src/main/scala/impatient_exercises/Chapter09.scala
@@ -1,10 +1,41 @@
 package impatient_exercises
 
+import java.io.PrintWriter
+
+import scala.io.Source
+
 object Chapter09 {
 
   /**
    * ===A class that provides a solution to Chapter 9 exercise 1  ===
    *
+   * 1. Write a Scala code snippet that reverses the lines in a file (making the last line the
+   *    first one, and so on).
    */
+  def reverseFile(path: String): Unit  = {
+    val reversedContents = Source.fromFile(path).getLines.toArray.reverse.mkString("\n")
+    new PrintWriter(path) {
+      write(reversedContents)
+      close()
+    }
+  }
+
+  /**
+   * ===A class that provides a solution to Chapter 9 exercise 2  ===
+   *
+   * 2. Write a Scala program that reads a file with tabs, replaces each tab with spaces so that
+   *    tab stops are at n-column boundaries, and writes the result to the same file.
+   */
+  def tabsToSpaces(path: String, spaces: Int): Unit  = {
+    val contents = Source.fromFile(path)
+      .mkString
+      .replaceAll("\t", (" " * spaces))
+
+    new PrintWriter(path) {
+      write(contents)
+      close()
+    }
+  }
+
 
 }
--- src/test/scala/impatient_exercises/Chapter09Test.scala
+++ src/test/scala/impatient_exercises/Chapter09Test.scala
@@ -1,10 +1,52 @@
 package impatient_exercises
 
+import java.io.{File, FileWriter, PrintWriter}
+
 import org.scalacheck.Gen
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 import org.scalatestplus.scalacheck.ScalaCheckDrivenPropertyChecks
 
+import scala.io.Source
+
 class Chapter09Test extends AnyFlatSpec with Matchers with ScalaCheckDrivenPropertyChecks {
 
+  def withFile(testCode: (File, FileWriter) => Any) {
+    val file = File.createTempFile("file", ".out") // create the fixture
+    val writer = new FileWriter(file)
+    try {
+      testCode(file, writer)
+    }
+    finally {
+      new File(file.getAbsolutePath).delete()
+    }
+  }
+
+  behavior of "reverseFile"
+
+  it should "reverse a file's contents" in withFile { (file, writer) =>
+    val contents = """these are the file's contents
+                     |it has several lines
+                     |and it should be reversed by reverseFile""".stripMargin
+    writer.write(contents)
+    writer.close()
+
+    Chapter09.reverseFile(file.getAbsolutePath)
+    Source.fromFile(file.getAbsolutePath, "UTF-8").getLines().toArray.head shouldEqual
+      "and it should be reversed by reverseFile"
+  }
+
+  behavior of "tabsToSpaces"
+
+  it should "replace all tab characters with specified number of spaces" in
+    withFile { (file, writer) =>
+
+      val contents = "1\t2\t3\t4\t5"
+      writer.write(contents)
+      writer.close()
+
+      Chapter09.tabsToSpaces(file.getAbsolutePath,6)
+      Source.fromFile(file.getAbsolutePath, "UTF-8").mkString shouldEqual
+        "1      2      3      4      5"
+  }
 }

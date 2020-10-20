b8acde9dc3c61eb65797b053a18f6fded214639b
Luis Guerrero <lcguerrerocovo@gmail.com>
2020-10-20 16:05:59 -0300

    more pattern matching solutions

--- src/main/scala/impatient_exercises/Chapter14.scala
+++ src/main/scala/impatient_exercises/Chapter14.scala
@@ -15,16 +15,53 @@ object Chapter14 {
 
   // **3.Using pattern matching, write a function swap that swaps the first two elements of an
   //     array provided its length is at least two.**
+  implicit def swapArray[T:ClassTag](arr: Array[T]): ArrayWithSwap[T] = new ArrayWithSwap[T](arr)
+
   class ArrayWithSwap[T:ClassTag](arr: Array[T]) {
     def swapFirstTwo: Array[T] = arr match {
       case Array(a,b, rest @ _*) if arr.length >= 2 => {
-        val buff = new ArrayBuffer[T]()
-        ((buff += b += a) ++ rest.toArray).toArray
+        Array(b,a) ++ rest.toArray[T]
       }
       case _ => arr
     }
   }
 
-  implicit def swapArray[T:ClassTag](arr: Array[T]): ArrayWithSwap[T] = new ArrayWithSwap[T](arr)
+  // **4.Add a case class Multiple that is a subclass of the Item class.
+  //     - For example:
+  //        `Multiple(10, Article("Blackwell Toaster", 29.95))`
+  //       describes ten toasters.
+  //     Of course, you should be able to handle any items, such as bundles or multiples, in the
+  //     second argument. Extend the price function to handle this new case.**
+  abstract class Item
+  case class Article(description: String, price: Double) extends Item
+  case class Bundle(description: String, discount: Double, items: Item*) extends Item
+  case class Multiple(quantity: Int, item: Item) extends Item
 
+  def price(it: Item): Double = it match {
+    case Article(_, p) => p
+    case Bundle(_, disc, its @ _*) =>
+      its.map {
+        case b: Bundle => price(b)
+        case Article(_,price) => price
+        case Multiple(quantity, item) => quantity * price(item)
+      }.sum - disc
+    case Multiple(quantity, item) => quantity * price(item)
+  }
+
+  // **5.One can use lists to model trees that store values only in the leaves. For example,
+  //     the list ((3 8) 2 (5)) describes the tree
+  //          •
+  //         /|\
+  //        • 2 •
+  //       /\   |
+  //      3  8  5
+  //     However, some of the list elements are numbers and others are lists. In Scala, you cannot
+  //     have heterogeneous lists, so you have to use a List[Any]. Write a leafSum function to
+  //     compute the sum of all elements in the leaves, using pattern matching to differentiate
+  //     between numbers and lists.**
+  def leafSum(tree: List[Any]): Int = tree match {
+    case Nil => 0
+    case (head: Int) :: tail => head + leafSum(tail)
+    case (head: List[Any]) :: tail => leafSum(head) + leafSum(tail)
+  }
 }
--- src/test/scala/impatient_exercises/Chapter14Test.scala
+++ src/test/scala/impatient_exercises/Chapter14Test.scala
@@ -16,8 +16,45 @@ class Chapter14Test extends AnyFlatSpec with Matchers with ScalaCheckDrivenPrope
 
   behavior of "Array[Any].swapFirstTwo"
 
-  it should "swap the first two elements of an array" in {
+  it should "swap the first two elements of an array of Int" in {
     Array(1,2,3,4,5).swapFirstTwo shouldEqual Array(2,1,3,4,5)
+  }
+
+  it should "swap the first two elements of an array of String" in {
+    Array("a","b","c","d","e").swapFirstTwo shouldEqual Array("b","a","c","d","e")
+  }
+
+  behavior of "Multiple"
+
+  val m1 = Multiple(10, Article("Blackwell Toaster", 29.95))
+  val m2 = Multiple(10,
+    Bundle("breakfast bundle", 15,
+      Article("toaster",30), Article("blender", 45)
+    )
+  )
+  val m3 = Multiple(10, m1)
+  val b1 = Bundle("super bundle", 1000, m1, m2, m3)
+
+  it should "compute price for a Multiple with an Article Item" in {
+    price(m1) shouldEqual 299.5d
+  }
+
+  it should "compute price for a Multiple with a Bundle Item" in {
+    price(m2) shouldEqual 600d
+  }
+
+  it should "compute price for a Multiple with a Multiple item" in {
+    price(m3) shouldEqual 2995
+  }
+
+  it should "compute price for a Bundle with all multiple items" in {
+    price(b1) shouldEqual 2894.5
+  }
+
+  behavior of "leafSum"
+
+  it should "add all leaves in list representation of a binary tree" in {
+    leafSum(List(List(3,8), 2, List(5))) shouldEqual 18
   }
 
 
